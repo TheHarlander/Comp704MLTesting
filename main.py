29af842a528d35e16717305eeed08f064da2ef83
Dean Harland <theharlander260@gmail.com>
2020-03-11 12:11:14 +0000

    Dean Harland
    added basic stuff

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pygame
 import random
 import math
+
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import ensemble
+from sklearn.metrics import mean_absolute_error
+from sklearn.externals import joblib
+from sklearn import linear_model
 from openpyxl import Workbook
 
 from time import sleep
@@ -61,8 +67,42 @@ errorRate = 1
 # Game restart
 gameOver = False
 
+#################################################################################################
+
 # Testing Data
-#df = pd.read_csv('MLDrivingData.csv')
+
+df = pd.read_csv("MLDrivingData.csv")
+
+items_df = pd.get_dummies(df, columns=['ObstacleXPos', 'CoinXPos'])
+
+# Create the X and Y arrays
+X = items_df.values
+y = df['PlayerPos'].values
+
+# Split the data set in a trining set (70%) and test set (30%)
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size= 0.3, random_state= 0)
+
+# Fit regression model
+model = ensemble.GradientBoostingRegressor(
+    n_estimators=1000,
+    learning_rate=0.1,
+    max_depth=6,
+    min_samples_leaf=9,
+    max_features=0.1,
+    loss='huber',
+    random_state=0
+)
+
+print('doing training ...')
+model.fit(X_train, y_train)
+print('done training ...')
+
+# Save trained model to pfl file
+joblib.dump(model,'testTrainData.pkl')
+
+# Find error rate
+mse = mean_absolute_error(y_train, model.predict(X_train))
+print("Training set mean absolute error: %.4f" % mse)
 
 
 ##################################################################################################
