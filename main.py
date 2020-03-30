a28b89c6db28e49880496b6cab6fb65bcc1d1dca
Dean Harland <theharlander260@gmail.com>
2020-03-30 13:38:18 +0100

    Maybe Final Commit. Cleaned up code. Added comments. Set sections.

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 import pygame
 import random
 import math
-
 import pickle
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn import ensemble
-from sklearn.metrics import mean_absolute_error
-
-from sklearn import linear_model
-from openpyxl import Workbook
-import joblib
 from time import sleep
 
 
@@ -30,20 +21,20 @@ playerImage = pygame.image.load('playercar.png')
 playerX = 375
 playerY = 750
 playerXSpeed = 0
-leftSpeed = -0.3
-rightSpeed = 0.3
+leftSpeed = -0.6
+rightSpeed = 0.6
 
 # Obstacle
 obstacleImage = pygame.image.load('cone.png')
 obstacleX = random.randint(200, 600)
 obstacleY = 0
-obstacleYSpeed = 0.4
+obstacleYSpeed = 0.8
 
 # Coins
 coinImage = pygame.image.load('coin.png')
 coinX = random.randint(200, 600)
 coinY = 0
-coinSpeed = 0.5
+coinSpeed = 1
 
 # Score
 scoreValue = 0
@@ -51,19 +42,12 @@ font = pygame.font.Font('freesansbold.ttf', 26)
 textX = 10
 textY = 10
 
-# Game Over
-gameOverFont = pygame.font.Font('freesansbold.ttf', 64)
-
 # Flavour
 roadMarking1X = 400
 roadMarking1Y = 0
 roadMarking2X = 400
 roadMarking2Y = 425
-roadMarkingSpeed = 0.4
-
-# Error rate 24 and it collides
-errorSize = 50
-errorRate = 1
+roadMarkingSpeed = 0.8
 
 # Game restart
 gameOver = False
@@ -73,14 +57,12 @@ gameOver = False
 # Testing data #
 # Linear Regression = LRtestTrainData.pkl
 # MulitLinear Regression = MLRtestTrainData.pkl
-# GBRegression = GBRtestTrainData.pkl
+# Gradient boosting Regression = GBRtestTrainData.pkl
+# Voter Regression = VRtestTrainData.pkl
 
-infile = open("MLRtestTrainData.pkl", 'rb')
+infile = open("VRtestTrainData.pkl", 'rb')
 pickleModel = pickle.load(infile)
 infile.close()
-
-
-
 
 
 ##################################################################################################
@@ -88,13 +70,6 @@ infile.close()
 def showScore(x, y):
     score = font.render("Score: " + str(scoreValue), True, (0, 0, 0))
     screen.blit(score, (x, y))
-
-
-# Renders game over text(score) to screen at x , y
-def gameOverText():
-    gameOverText = gameOverFont.render("Score: " + str(scoreValue), True, (0, 0, 0))
-    screen.blit(gameOverText, (280, 300))
-
 
 # Draw player at x , y
 def player(x, y):
@@ -140,11 +115,24 @@ while running:
     pygame.draw.rect(screen, (30, 30, 30), [140, 0, 10, 850])
     pygame.draw.rect(screen, (100, 140, 100), [650, 0, 200, 850])
     pygame.draw.rect(screen, (30, 30, 30), [650, 0, 10, 850])
-    pygame.draw.rect(screen, (200, 200, 200), [roadMarking1X, roadMarking1Y, 20, 100])
-    pygame.draw.rect(screen, (200, 200, 200), [roadMarking2X, roadMarking2Y, 20, 100])
+    pygame.draw.rect(screen, (200, 200, 200), [round(roadMarking1X), round(roadMarking1Y), 20, 100])
+    pygame.draw.rect(screen, (200, 200, 200), [round(roadMarking2X), round(roadMarking2Y), 20, 100])
 
     ##############################################################################
-    X = [[obstacleX, obstacleY, coinX, coinY]]
+   # Data prediction and movement
+
+    nearBool = 0
+    if obstacleY >= 600:
+        nearBool = 1
+    elif obstacleY < 600:
+        nearBool = 0
+
+    # LR =  X = [[coinX]]
+    # MLR = X = [[obstacleX,obstacleY, coinX, coinY]]
+    # GBR + VR =  X = [[obstacleX,obstacleY, coinX, coinY, nearBool]]
+
+    X = [[obstacleX,obstacleY, coinX, coinY, nearBool]]
+
     ynew = pickleModel.predict(X)
 
     if playerX > ynew:
@@ -176,7 +164,7 @@ while running:
                 roadMarkingSpeed -= 0.1
                 coinSpeed -= 0.1
 
-        # Maybe add some logic here for retaining speed when released, maybe halves?
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerXSpeed = 0
@@ -198,18 +186,6 @@ while running:
         obstacleX = random.randint(175, 625)
         scoreValue += 1
 
-        # Increase difficulty
-        obstacleYSpeed += 0.01
-        roadMarkingSpeed += 0.01
-        coinSpeed += 0.01
-
-        # Decrease error size
-        errorSize = errorSize - errorRate
-
-
-   # if scoreValue == 10:
-       # print("10 score met spawn new obstacle")
-
     # Road markings + re spawn
     roadMarking1Y += roadMarkingSpeed
     roadMarking2Y += roadMarkingSpeed
@@ -225,13 +201,9 @@ while running:
     coinCollision = isCollision(coinX, coinY, playerX, playerY)
     if coinCollision:
 
-
-
-
         coinY = -50
         coinX = random.randint(175, 625)
         scoreValue += 1
-
 
     if coinY >= 850:
         coinY = -50
@@ -244,15 +216,14 @@ while running:
         playerXSpeed = 0
         roadMarkingSpeed = 0
         coinSpeed = 0
-        gameOverText()
         gameOver = True
 
     if gameOver == True:
         obstacleY = -50
         obstacleX = random.randint(175, 625)
-        obstacleYSpeed = 0.4
-        roadMarkingSpeed = 0.4
-        coinSpeed = 0.5
+        obstacleYSpeed = 0.8
+        roadMarkingSpeed = 0.8
+        coinSpeed = 1
         coinY = -50
         coinX = random.randint(175, 625)
         scoreValue = 0
@@ -261,7 +232,7 @@ while running:
         gameOver = False
 
     # Update image locations
-    player(playerX, playerY)
+    player(round(playerX), round(playerY))
     obstacle(round(obstacleX), round(obstacleY))
     coin(round(coinX), round(coinY))
     showScore(round(textX), round(textY))
